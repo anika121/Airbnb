ce768bfb075f39c28e9e548382b2e952c0be370c
aaravagarwal <aarav.agarwal.000@gmail.com>
2021-01-28 16:02:54 -0500

    Finish button

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
 import {Pressable, Text, View} from 'react-native';
 
@@ -8,53 +9,56 @@ const GuestsScreen = () => {
 	const [children, setChildren] = useState(0)
 	const [infants, setInfants] = useState(0)
 
+	const navigation = useNavigation()
+
     return (
-        <View>
-            <View
-                style={styles.row}>
-                <View>
-                    <Text style={{fontWeight: '600', fontSize: 16}}>
-                        Adults
-                    </Text>
-                    <Text style={{color: '#8d8d8d'}}>Ages 13 and above</Text>
-                </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <Pressable
-                        onPress={() => setAdults(Math.max(0, adults - 1))}
-                        style={styles.button}>
-                        <Text style={{ fontSize: 20, color: '#474747' }}>-</Text>
-                    </Pressable>
-                    <Text style={{ marginHorizontal: 14, fontSize: 17 }}>{adults}</Text>
-                    <Pressable
-                        onPress={() => setAdults(adults + 1)}
-                        style={styles.button}>
-                        <Text style={{ fontSize: 20, color: '#474747' }}>+</Text>
-                    </Pressable>
-                </View>
-            </View>
-			<View
-                style={styles.row}>
-                <View>
-                    <Text style={{fontWeight: '600', fontSize: 16}}>
-                        Children
-                    </Text>
-                    <Text style={{color: '#8d8d8d'}}>Ages 2 to 12</Text>
-                </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <Pressable
-                        onPress={() => setChildren(Math.max(0, children - 1))}
-                        style={styles.button}>
-                        <Text style={{ fontSize: 20, color: '#474747' }}>-</Text>
-                    </Pressable>
-                    <Text style={{ marginHorizontal: 14, fontSize: 17 }}>{children}</Text>
-                    <Pressable
-                        onPress={() => setChildren(children + 1)}
-                        style={styles.button}>
-                        <Text style={{ fontSize: 20, color: '#474747' }}>+</Text>
-                    </Pressable>
-                </View>
-            </View>
-			<View
+        <View style={{justifyContent: 'space-between', height: '100%'}}>
+			<View>
+				<View
+					style={styles.row}>
+					<View>
+						<Text style={{fontWeight: '600', fontSize: 16}}>
+							Adults
+						</Text>
+						<Text style={{color: '#8d8d8d'}}>Ages 13 and above</Text>
+					</View>
+					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+						<Pressable
+							onPress={() => setAdults(Math.max(0, adults - 1))}
+							style={styles.button}>
+							<Text style={{ fontSize: 20, color: '#474747' }}>-</Text>
+						</Pressable>
+						<Text style={{ marginHorizontal: 14, fontSize: 17 }}>{adults}</Text>
+						<Pressable
+							onPress={() => setAdults(adults + 1)}
+							style={styles.button}>
+							<Text style={{ fontSize: 20, color: '#474747' }}>+</Text>
+						</Pressable>
+					</View>
+				</View>
+				<View
+					style={styles.row}>
+					<View>
+						<Text style={{fontWeight: '600', fontSize: 16}}>
+							Children
+						</Text>
+						<Text style={{color: '#8d8d8d'}}>Ages 2 to 12</Text>
+					</View>
+					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+						<Pressable
+							onPress={() => setChildren(Math.max(0, children - 1))}
+							style={styles.button}>
+							<Text style={{ fontSize: 20, color: '#474747' }}>-</Text>
+						</Pressable>
+						<Text style={{ marginHorizontal: 14, fontSize: 17 }}>{children}</Text>
+						<Pressable
+							onPress={() => setChildren(children + 1)}
+							style={styles.button}>
+							<Text style={{ fontSize: 20, color: '#474747' }}>+</Text>
+						</Pressable>
+					</View>
+				</View>
+				<View
                 style={styles.row}>
                 <View>
                     <Text style={{fontWeight: '600', fontSize: 16}}>
@@ -76,7 +80,26 @@ const GuestsScreen = () => {
                     </Pressable>
                 </View>
             </View>
-        </View>
+			</View>
+			<View>
+				<Pressable style={{
+					marginBottom: 20, 
+					backgroundColor: '#f15454', 
+					alignItems: 'center',
+					justifyContent: 'center',
+					height: 55,
+					marginHorizontal: 20,
+					borderRadius: 10
+				}} onPress={() => navigation.navigate('Home', {
+					screen: "Explore",
+					params: {
+						screen: 'SearchResults'
+					}
+				})}>
+					<Text style={{ fontSize: 18, color: 'white' }}>See Available Stays...</Text>
+				</Pressable>
+			</View>
+		</View>
     );
 };
 

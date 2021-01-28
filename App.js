7ace9b52e4da077cd377903e575ad7da3e966268
aaravagarwal <aarav.agarwal.000@gmail.com>
2021-01-28 15:26:58 -0500

    navigation implemented

--- App.js
+++ App.js
@@ -6,32 +6,19 @@
  * @flow strict-local
  */
 
+import 'react-native-gesture-handler'
 import React from 'react';
 import {
-    SafeAreaView,
     StatusBar,
 } from 'react-native';
 
-import Post from './src/components/Post';
-import SearchResultsScreen from './src/screens/SearchResults';
-import HomeScreen from './src/screens/Home';
-import DestinationSearchScreen from './src/screens/DestinationSearch';
+import Router from './src/navigation/Router';
 
-import feed from './assets/data/feed'
-import GuestsScreen from './src/screens/Guests';
-
-const post2 = feed[1];
 const App: () => React$Node = () => {
     return (
         <>
             <StatusBar barStyle="dark-content" />
-            <SafeAreaView>
-				{/* <HomeScreen /> */}
-				{/* <Post post={post2} /> */}
-				{/* <SearchResultsScreen /> */}
-				{/* <DestinationSearchScreen /> */}
-				<GuestsScreen />
-			</SafeAreaView>
+            <Router />
         </>
     );
 };

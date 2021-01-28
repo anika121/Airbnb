c2a934eee7d753a76ed94e3deb98744a152ee364
aaravagarwal <aarav.agarwal.000@gmail.com>
2021-01-28 16:13:27 -0500

    Explore navigator

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomeScreen from '../screens/Home'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ExploreNavigator from './ExploreNavigator'
 
 import Fontisto from 'react-native-vector-icons/Fontisto'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
@@ -17,7 +18,7 @@ const HomeTabNavigator = () => {
 		}}>
 			<Tab.Screen
 				name={"Explore"}
-				component={HomeScreen}
+				component={ExploreNavigator}
 				options={{
 					tabBarIcon: ({ color }) => (
 						<Fontisto name="search" size={25} color={color} />
